dcdcf6bca58c97cb683851a0929d38bac3469355
Rohini <rohini_subramanian@outlook.com>
2022-08-22 23:27:15 +0800

    Create durable subscription

--- practice/src/main/java/com/spring/integration/practice/jms/basics/DestinationJMS.java
+++ practice/src/main/java/com/spring/integration/practice/jms/basics/DestinationJMS.java
@@ -33,20 +33,55 @@ public class DestinationJMS {
     }
 
     // Use an overloaded method for topic & Durable Subscriptions
+    public TopicSubscriber createTopicSubscriber(Session session, Destination dst, MessageListener messageListener, String subscriptionName) throws JMSException {
+        // provide name of subscription
+        TopicSubscriber topicSubscriber = session.createDurableSubscriber((Topic) dst,subscriptionName);
+        topicSubscriber.setMessageListener(messageListener);
+        return topicSubscriber;
+    }
 
     public static void main(String[] args) throws JMSException {
         DestinationJMS app = new DestinationJMS();
         ConnectionFactory cf = app.createConnectionFactory();
         Connection conn = app.createConnection(cf);
+        conn.setClientID("MyUniqueClientID"); // must define client ID on the connection
         Session session = app.createMsgSession(conn);
 
         // Define destination type here and pass in as argument
+        /*
         Destination dst = session.createQueue("QUEUE_1");
         app.createMsgConsumer(session,dst,msg->{
             System.out.println(msg);
         });
         app.sendMsgToDestination("hello world",session,dst);
+        */
+        Destination dst2 = session.createTopic("TOPIC_1");
+        TopicSubscriber subscriber = app.createTopicSubscriber(session,dst2,msg->{
+            System.out.println(msg);
+        },"test-subscription");
+        app.sendMsgToDestination("hello TOPIC",session,dst2);
+
         conn.start();
+
+        // Free up resources
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run(){
+                try{
+                    super.run();
+                    conn.stop();
+                    subscriber.close(); // when we close topicSubscriber = keep any messages for me until I connect again
+                    session.close();
+                    conn.close();
+
+                    // If you no longer want to keep messages on the broker after disconnecting, need to close session
+                    // by unsubscribing using the same subscription name as earlier
+                    session.unsubscribe("test-subscription");
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
