d961eb0a24f6cb68e4320864de5e594404fce321
Rohini <rohini_subramanian@outlook.com>
2022-08-24 22:50:29 +0800

    Synchronous messaging using JMS

--- practice/src/main/java/com/spring/integration/practice/jms/production/MyMessageSender.java
+++ practice/src/main/java/com/spring/integration/practice/jms/production/MyMessageSender.java
@@ -2,10 +2,10 @@ package com.spring.integration.practice.jms.production;
 
 import javax.jms.JMSException;
 
-public class Message extends AbstractApplication {
+public class MyMessageSender extends AbstractApplication {
     public static void main(String[] args) throws JMSException {
         // Sending 100 messages is done in the constructor
-        Message sender = new Message();
+        MyMessageSender sender = new MyMessageSender();
         sender.shutdown();
     }
 }
